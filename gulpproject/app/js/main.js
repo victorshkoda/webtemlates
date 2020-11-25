7729b0cc290756bfdf14b0fe8b4261c4105721b5
victorshkoda <victorshkoda@gmail.com>
2020-11-26 00:33:16 +0600

    added imagemin gulp

--- gulpproject/app/js/main.js
+++ gulpproject/app/js/main.js
@@ -4,3 +4,8 @@ async function start(){
     return await Promise.resolve("Working async")
 }
 start().then(console.log)
+
+const foo = () => {
+    console.log('From fun function')
+}
+foo()
--- gulpproject/gulpfile.js
+++ gulpproject/gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, watch, parallel} = require('gulp')
+const {src, dest, watch, parallel, series} = require('gulp')
 const scss = require('gulp-sass')
 const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create()
@@ -6,6 +6,9 @@ const ugify = require('gulp-uglify-es').default
 const babel = require('gulp-babel')
 const sourcemaps = require('gulp-sourcemaps')
 const strip = require('gulp-strip-comments')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const del = require('del')
 
 function browsersync() {
     browserSync.init({
@@ -14,6 +17,26 @@ function browsersync() {
         },
         port: 3003
     })
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
 }
 
 // !!! Use if js with promices or generators
@@ -39,9 +62,10 @@ function scripts() {
 
 function styles() {
     return src('app/scss/style.scss')
+        .pipe(sourcemaps.init())
         .pipe(scss({outputStyle: 'compressed'}))
-        .pipe(concat('syle.min.css'))
-        .pipe(sourcemaps.write('.'))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({overrideBrowserslist: ['last 10 version'], grid: true}))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream())
 }
@@ -52,8 +76,21 @@ function watching(){
    watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
+function build(){
+    return src([
+        'app/css/**/*.min.css',
+        'app/js/**/*.min.js',
+        'app/fonts/**/*',
+        'app/**/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
 exports.styles = styles
 exports.scripts = scripts
 exports.watching = watching
 exports.browsersync = browsersync
-exports.default = parallel(browsersync, watching)
+exports.images = images
+exports.cleanDist = cleanDist
+exports.build = series(styles, scripts, cleanDist, images, build)
+exports.default = parallel(styles, scripts, browsersync, watching)
